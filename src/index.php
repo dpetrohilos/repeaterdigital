0a61e94652d3b68a867a5a38805bec22d8e829a9
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-30 20:43:25 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 <div class="wrapper-full">
     <section class="row">
-        <div class="cover">
         
 <div class="col-lg-3 col-lg-offset-3">
      <img class="responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Graphic Poster" class="multiply" width="469" height="476" />   
@@ -13,7 +12,6 @@
      <img class="responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo-red.svg" alt="Logo" class="logo-red" width="469" height="486" />   
             
 </div>    
-        </div>
     </section>
     </div>
     
