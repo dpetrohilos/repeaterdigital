b0aaf27fb5b6836394a88313a0ed6f16703a46d6
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-30 19:44:04 -0500

    1 build

--- src/index.php
+++ src/index.php
@@ -1,5 +1,28 @@
 <?php get_header(); ?>
-
+<div class="cover">
+<div class="wrapper-full">
+    <section class="row">
+    
+<div class="col-lg-3 col-lg-offset-3">
+     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Graphic Poster" class="multiply" width="116">   
+            
+</div>
+        
+        
+<div class="col-lg-3 col-lg-offset-3 bg-red">
+     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo-red.svg" alt="Logo" class="logo-red" width="116">   
+            
+</div>    
+    
+    </section>
+    </div>
+    
+</div>    
+    <div class="hero"> 
+    
+    
+    
+    </div>    
 	<main role="main" aria-label="Content">
 		<!-- section -->
 		<section>
