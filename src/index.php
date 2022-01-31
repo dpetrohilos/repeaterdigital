d0de7ba0ca6641af4ee32d5ea4d6324f88c87e64
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-30 20:36:45 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <div class="cover">
 <div class="wrapper-full">
     <section class="row">
-    
+        
 <div class="col-lg-3 col-lg-offset-3">
      <img class="responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Graphic Poster" class="multiply" width="469" height="476" />   
             
