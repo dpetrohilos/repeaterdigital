67f09c091aff01064eeb364809a8004f571f1049
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-30 19:48:34 -0500

    1

--- src/index.php
+++ src/index.php
@@ -4,13 +4,13 @@
     <section class="row">
     
 <div class="col-lg-3 col-lg-offset-3">
-     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Graphic Poster" class="multiply" width="116">   
+     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Graphic Poster" class="multiply" width="290" height="500">   
             
 </div>
         
         
 <div class="col-lg-3 col-lg-offset-3 bg-red">
-     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo-red.svg" alt="Logo" class="logo-red" width="116">   
+     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo-red.svg" alt="Logo" class="logo-red" width="290" height="280">   
             
 </div>    
     
