8c83f39ba5147983b47d5d148b9df3540ad42b76
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-31 16:21:45 -0500

    1

--- src/index.php
+++ src/index.php
@@ -1,11 +1,9 @@
 <?php get_header(); ?>
 <div class="grid-container cover">
-
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/circle.svg" alt="Graphic Poster" class="multiply" width="400"/>   
     <b class="border"></b> 
 </div>
-
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Graphic Poster" class="multiply" width="400"/>   
     <b class="border"></b> 
@@ -14,25 +12,18 @@
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Logo_Red.svg" alt="Logo" class="logo-red" width="400"  />   
             
 </div>
-    </div>
-
 <div>
     <h2>Repeater</h2>            
 </div>
 <div>
     <h2>Digital</h2>
 
-</div>
-    
-<div>
+</div>   
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/triangle.svg" alt="Logo" class="logo-red" width="150"  />   
             
 </div>
-    </div>
-    </div>
-
-    </div>
+</div>
 
 	<main role="main" aria-label="Content">
 		<!-- section -->
