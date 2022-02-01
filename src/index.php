6cb6a65ea0f143785ffd98941f8a91fd92aff852
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-31 23:12:56 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -20,7 +20,11 @@
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/RD_Art_Element.svg" alt="Logo" class="logo-red" width="160"  />   
     </p>     
 </div>
-      
+    
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_2.jpg" alt="Graphic Poster"  width="400"/>   
+</div>  
+ 
     
 </div>
 
