7f603b7ff792aacd564c254f502d6cfeec5236af
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-31 23:12:25 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -20,22 +20,7 @@
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/RD_Art_Element.svg" alt="Logo" class="logo-red" width="160"  />   
     </p>     
 </div>
-    
-<div>
-<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_2.jpg" alt="Graphic Poster"  width="400"/>   
-</div>  
-<div>
-<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_3.jpg" alt="Graphic Poster" width="400"/>   
-</div>
-<div>
-<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_4.jpg" alt="Graphic Poster"  width="800"/>   
-</div>
-    
-<div>
-    <h1>We Change <span>Narratives</span></h1>
-</div>
-<div>
-    <p><strong>Repeater Digital</strong> is a full stack creative collaborative, democratically managed by grassroots activists in the DC metro area. We design effective messaging campaigns by asking the right questions of our clients, as well as target audiences. </p></div>    
+      
     
 </div>
 
