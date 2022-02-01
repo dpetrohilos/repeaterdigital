3536daea36eaee653668b227e334150cb7a5f119
dpetrohilos <dylanpetrohilos@gmail.com>
2022-01-31 23:14:09 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,29 +2,39 @@
 <div class="grid-container cover front">
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/circle.svg" alt="Graphic Poster" class="multiply" width="400"/>   
-    <b class="border"></b> 
 </div>
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Graphic Poster" class="multiply" width="400"/>   
-    <b class="border"></b> 
 </div>      
 <div>
-<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Logo_Red.svg" alt="Logo" class="logo-red" width="400"  />   
-            
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Logo_Red.svg" alt="Logo" class="logo-red" width="400"  />         
 </div>
+    
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Triangle.svg" alt="Logo" class="logo-red" width="40"  />   
             
 </div>   
-<div><p>
+<div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/RD_Art_Element.svg" alt="Logo" class="logo-red" width="160"  />   
-    </p>     
+    
 </div>
     
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_2.jpg" alt="Graphic Poster"  width="400"/>   
-</div>  
- 
+</div>
+    
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_3.jpg" alt="Graphic Poster" width="400"/>   
+</div>
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_4.jpg" alt="Graphic Poster"  width="800"/>   
+</div>
+    
+<div>
+    <h1>We Change <span>Narratives</span></h1>
+</div>
+<div>
+    <p><strong>Repeater Digital</strong> is a full stack creative collaborative, democratically managed by grassroots activists in the DC metro area. We design effective messaging campaigns by asking the right questions of our clients, as well as target audiences. </p></div>    
     
 </div>
 
