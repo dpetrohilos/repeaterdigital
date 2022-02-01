3955e0f1159c7a5e3171b57276ddbfeb9b5a75aa
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-01 13:58:04 -0500

    1

--- src/index.php
+++ src/index.php
@@ -37,7 +37,23 @@
 </div>
 <div>
     <p><strong>Repeater Digital</strong> is a full stack creative collaborative, democratically managed by grassroots activists in the DC metro area. We design effective messaging campaigns by asking the right questions of our clients, as well as target audiences. </p></div>    
-    
+    <a class="btn" href="#" title="Contact Us">Lets Talk</a> <a href="#" Title="Our Work" Class="link">Case Study</a>
+</div>
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Triangle.svg" alt="Logo" class="logo-red" width="40"  />   
+            
+</div>   
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_5.svg" alt="Graphic Poster"  width="800"/>   
+</div>
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_6.svg" alt="Graphic Poster"  width="800"/>   
+</div>
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_7.jpg" alt="Graphic Poster"  width="800"/>   
+</div>
+<div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_8.jpg" alt="Graphic Poster"  width="800"/>   
 </div>
 
 	
