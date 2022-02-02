6ccb6964e09054f048eac14bd5421f0cc8a83bdf
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-01 19:42:42 -0500

    Front Page Updates

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="grid-container cover front">
-<div class="animate__animated animate__zoomInDown">
+<div class="animate__animated animate__zoomInDown multiply">
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/circle.svg" alt="Graphic Poster" class="multiply" width="600"/>   
 </div>
 <div>
@@ -54,6 +54,112 @@
 <div>
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_8.jpg" alt="Graphic Poster"  width="800"/>   
 </div>
+<div class="content">
+    <h2>Vision</h2>
+    <p>We are a worker owned, self-managed communications consulting firm that specializes in strategic digital and visual communication. </p>
+
+<p>Repeater Digital strives to isolate the signal from the noise. We use the power of storytelling to transmit and amplify the voices of our partners across the shared global digital community. </p>
+    
+    
+    
+</div>
+    
+    
+<div class="content">    
+    <blockquote>
+    Activists, organizers and punk rockers.
+    </blockquote>    
+    
+</div>  
+    
+     
+<div class="content">    
+    <blockquote>
+    Activists, organizers and punk rockers.
+    </blockquote>    
+    
+</div>     
+    
+    
+   <div class="content">    
+<h3>Skills</h3> 
+<ul>
+<li>Mass Action Strategy</li>
+<li>Brand Identity </li>
+<li>WordPress Dev. </li>
+<li>Art Direction </li>
+<li>UI/UX Design </li>
+<li>Social Media </li>
+<li>Video Editing </li>
+<li>Messaging </li>
+<li>Story Telling </li>
+<li>Drupal Dev.</li>
+<li>Logo Design </li>
+<li>Social Media Campaigns</li>
+<li>Guerilla Marketing/PR</li>
+<li>Public Awareness</li>
+<li>Mass Communications</li> 
+</ul>
+    </div>
+    
+<div class="content">
+    
+    <h3>Cultural Workers</h3>
+    <p>Bere peribus. Maio qui aris aliqui blanis ium ulla es et ex earum elluptaqui conestios acest experibusdae excernam hicia nos autendem.</p>
+<p>
+Lique volupta dunt moluptae que molendi tiisse volore quodit ipsande nihitincium elit expel mo bla volest id molupti odisquibus reptium dolorro eos delit liqui con repudisqui recea nam quid maio. 
+</p>
+
+<p>
+Neque oditatatem dolupta tusapici dolorei cipsum cone quatio optassi magnatus, ad quam fuga. Et eiunt labore erum excerum quis asperehenim fuga. Abore et ut lab</p>
+</div>
+    
+    <div class="content">
+    
+    
+    <blockquote>Part of resistance culture.</blockquote>
+</div>
+    <div>
+<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_5.jpg" alt="Graphic Poster"  width="1200"/>   
+    
+    </div>
+
+    
+    <div class="content">
+    
+        <h4>Our Clients</h4>
+        <p>Neque oditatatem dolupta tusapici dolorei cipsum cone quatio optassi magnatus, ad quam fuga. Et eiunt labore erum excerum quis asperehenim fuga. Abore et ut lab</p>
+        
+        </div>
+    
+    <div class="content clients">
+<a href="#client"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Client1.jpg" alt="Graphic Poster"  width="1200"/></a>   
+<a href="#client"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Client2.jpg" alt="Graphic Poster"  width="1200"/></a>   
+<a href="#client"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Client3.jpg" alt="Graphic Poster"  width="1200"/></a>   
+<a href="#client"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Client4.jpg" alt="Graphic Poster"  width="1200"/></a>   
+        
+    </div>
+    
+<div class="content">
+    <h5>Testimonial</h5>
+    <q>“For years the individuals that make up Repeater Digital have lived and breathed progressive communications—from building grassroots communities to transforming popular opinion—their work has had resounding global reach and impact. From graphic design to media relations and everything in between, Repeater has what it takes to educate, amplify, and inspire.”</q>
+    
+    <cite>—Ryan Fletcher, Director of Movement Media</cite>
 
 </div>
+  
+<div class="content Clients">
+<a href="#client"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work1.jpg" alt="Graphic Poster"  width="1200"/></a>   
+<a href="#client"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work2.jpg" alt="Graphic Poster"  width="1200"/></a>   
+<a href="#client"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work3.jpg" alt="Graphic Poster"  width="1200"/></a>   
+    
+</div>    
+    
+    
+</div>
+
+
+
+
+
 <?php get_footer(); ?>
