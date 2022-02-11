02c97a5ef0d00d1ddac4cddbecff763c40113603
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-10 23:19:32 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -52,7 +52,7 @@
 </div>      
 <div class="subgrid">    
 <div class="bg_red"></div>
-<div><img src="<?php class="multiply" echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_12.jpg" alt="Graphic Poster"  width="1200"/>   
+<div><img class="multiply" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_12.jpg" alt="Graphic Poster"  width="1200"/>   
 </div>
 <div class="content">
     <h3>We offer</h3>
