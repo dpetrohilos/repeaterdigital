1478fe479d19101443d01b4065c0f017fbb7acef
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-14 15:15:30 -0500

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,7 +1,11 @@
 			<!-- footer -->
 			<footer class="footer row" role="contentinfo">
-<div class="col-4 offset-lg-2">
-				<!-- copyright -->
+<div class="offset-lg-2 offset-sm-1 offset-xs-1 offset-md-1 col-lg-4 col-xs-10 col-sm-10 col-md-4">
+				
+    <p>Repeater Digital is a full stack creative  collaborative, democratically managed by creative professionals with decades of experience.
+    </p>
+    
+    <!-- copyright -->
 			    <?php
 				    printf( '<p class="copyright">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . '</p>',
 				    	date( 'Y'),
@@ -13,10 +17,8 @@
 				<!-- /copyright -->
                 </div>
 
-<div class="col-6">
-    
-    			<!-- /put contact form here -->
-
+<div class="col-lg-4 col-xs-12 col-sm-12 col-md-7">
+    <?php echo do_shortcode('[contact-form-7 id="8" title="Contact form 1"]'); ?>
     
 </div>
 </footer>
