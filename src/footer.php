0ea626f8d5b69587f9e4f08f93b989715419bcbd
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-14 15:43:25 -0500

    footer beta

--- src/footer.php
+++ src/footer.php
@@ -2,6 +2,10 @@
 			<footer id="contact" class="footer" role="contentinfo"><section class="row">
 <div class="col-lg-offset-3 col-xs-offset-1 col-md-offset-3 col-md-offset-3 col-lg-4 col-xs-10 col-sm-10 col-md-4">
 				
+    <div class="footer-logo">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1080 1080"><path d="M629.69,205.36v0H346.56c-97.84,0-177.44,79.6-177.44,177.44,0,81.41,55.14,150.14,130,170.94a.14.14,0,0,1,.09.19c-51.71,136.54-87.82,248-103.15,291-3.58,10,3.62,21,14.28,21,71.16,0,415.09-.08,415.09-.08C819.89,870,965.82,710.79,957.78,521.4,950.35,346.47,806.19,206.34,629.69,205.36ZM473.45,560.2V824.61H238.67C265.09,756.8,315.45,627.16,341,560.06M473.45,519H346.56c-75.1,0-136.2-61.1-136.2-136.2s61.1-136.2,136.2-136.2H473.45ZM627.83,824.61H514.68v-578H627.83c159.37,0,289,129.65,289,289S787.2,824.61,627.83,824.61Z"/><circle cx="694.99" cy="545.96" r="20.78"/><circle cx="362.68" cy="382.77" r="20.78"/><path d="M821.12,838.81H810.69V836H834.5v2.78H824.07v26.77h-3Z"/><path d="M865.16,840.38l-11,16.21h-.84l-11.06-16.42v25.41h-3V836h3.63l10.81,16.29L864.4,836h3.72v29.55h-3Z"/></svg>
+    </div>
+    
     <p>Repeater Digital is a full stack creative  collaborative, democratically managed by creative professionals with decades of experience.
     </p>
     
