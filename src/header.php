05353fd7c52e3ef405c96d45bfa828ef2b3f9523
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-15 00:00:38 -0500

    1

--- src/header.php
+++ src/header.php
@@ -34,9 +34,9 @@
 			<header class="header row start-xs" role="banner">
 					<!-- logo -->
 					<div class="center col start-xs text-center">
-						<a href="<?php echo esc_url( home_url() ); ?>">
+						<a class="logo" href="<?php echo esc_url( home_url() ); ?>">
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Logo" class="logo-img" width="116">
+							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Repeater Digital" class="logo-img" width="116">
 						</a>
 					</div>
                 <!-- /logo -->
