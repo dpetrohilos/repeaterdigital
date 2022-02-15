616798eaeee450e6d2fe9a4b9c0dbaf771b7b810
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-15 14:23:42 -0500

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -15,10 +15,10 @@
     
                 </div>
 
-<div class="col-lg-4 col-xs-12 col-sm-12 col-md-5 content">
+<div class="col-lg-4 col-xs-12 col-sm-6 col-md-5 content">
     <?php echo do_shortcode('[contact-form-7 id="8" title="Contact form 1"]'); ?>
     
-                </div></section><section class="row text-center"><div class="col-lg-offset-4 col-xs-offset-1 col-md-offset-2 col-md-offset-3 col-lg-4 col-xs-10 col-sm-10 col-md-6">    <!-- copyright -->
+                </div></section><section class="row text-center"><div class="col-lg-offset-4 col-xs-offset-1 col-md-offset-2 col-md-offset-3 col-lg-4 col-xs-10 col-sm-6 col-md-6">    <!-- copyright -->
 			    <?php
 				    printf( '<p class="copyright">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . '</p>',
 				    	date( 'Y'),
