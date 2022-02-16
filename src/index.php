825a9a76d07001958205c213ab517c1c00000c1c
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-16 02:56:17 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -11,12 +11,18 @@
     
 </div>       
 <div>
-<img class="multiply animate__fadeInDown animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="" width="1200"/>   
+    
+    
+<picture class="animate__fadeIn animate__animated multiply">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_1.jpg" alt="Movement Media, thankyou card." width="1200" height="1200">
+</picture>
+        
 </div>
 <div>
 <svg alt="repeater digital red and black text" class="repeaterdesign animate__fadeIn animate__animated" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 87.75 116.05"><defs><clipPath id="a"><rect x="1.22" y="35.7" width="86.53" height="80.13" style="fill:none"/></clipPath></defs><g style="clip-path:url(#a)"><path d="M81.75,38.23A1.26,1.26,0,0,1,83,39.62a1.17,1.17,0,0,1-1.2,1.3H79.44V38.23Zm4.89,8.48-2.91-3.77a3.37,3.37,0,0,0,2.37-3.38c0-2-1.49-3.86-4.35-3.86H76.28V46.9h3.16V43.27h1.11l2.61,3.63h3.48ZM74.8,44.23H69V42.54h5.3V40H69v-1.6h5.62V35.7H65.84V46.9h9ZM58.36,46.9h3.15V38.31h3.28V35.7h-9.7v2.61h3.27Zm-7.12-4.49H48.37l1.44-3.57ZM53,46.9h3.4l-5-11.2h-3.2l-5,11.2h3.36l.74-1.84h5Zm-9.93-2.67H37.27V42.54h5.29V40H37.27v-1.6h5.61V35.7H34.15V46.9h9Zm-14.55-.35c3,0,4.5-1.81,4.5-4.1s-1.49-4.08-4.5-4.08H23.12V46.9h3.17v-3Zm-2.27-5.46h2.27a1.32,1.32,0,0,1,1.33,1.41,1.28,1.28,0,0,1-1.33,1.38H26.29Zm-4.64,5.81H15.81V42.54h5.3V40h-5.3v-1.6h5.62V35.7H12.69V46.9h9Zm-15-6a1.26,1.26,0,0,1,1.23,1.39,1.17,1.17,0,0,1-1.2,1.3H4.39V38.23Zm4.9,8.48L8.68,42.94A3.38,3.38,0,0,0,11,39.56c0-2-1.48-3.86-4.35-3.86H1.22V46.9H4.39V43.27h1.1L8.1,46.9h3.49Z" style="fill:#e52227"/><path d="M87.43,107.3H76.23v8.53h2.69v-5.36h8.51Zm-6.7-5.77V98.66l3.57,1.44Zm-4.5,1.81v3.39l11.2-5v-3.2l-11.2-5v3.36l1.84.73v5Zm0-14.93v3.15h8.59v3.28h2.61v-9.7H84.82v3.27Zm0-4.32h11.2V80.94H76.23Zm9.71-5a5.58,5.58,0,0,0,1.81-4.4,5.74,5.74,0,0,0-5.93-6.12c-2.93,0-5.89,1.68-5.89,6.11,0,2.16.77,4.11,2.19,4.81h5V74H80.62v2.83H79.18a3.83,3.83,0,0,1-.58-2.16,2.94,2.94,0,0,1,3.22-3c2.19,0,3.28,1.18,3.28,3a3.68,3.68,0,0,1-1,2.56ZM76.23,67.21h11.2V64.06H76.23Zm8.56-12v1.66a2.56,2.56,0,0,1-2.85,2.72c-1.53,0-3-.7-3-2.72V55.18ZM87.43,52H76.23v4.8c0,4,2.59,5.86,5.68,5.86s5.52-1.91,5.52-5.86Z" style="fill:#444545"/></g></svg>    
 </div>
-    
 <div class="bg-red animate__fadeIn animate__animated"></div>
     
     </div>
@@ -24,19 +30,36 @@
 <div class="grid-item grid-item-2">
 
 <div>
-<img class="animate__fadeIn animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_2.jpg" alt="Card Deck Design" width="900"  />      
-</div>       
+<picture class="animate__fadeIn animate__animated">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_2.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_2.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_2.jpg" alt="Movement Media, thankyou card." width="1200" height="1200">
+</picture></div>       
 <div>
-<img class="animate__fadeIn animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_3.jpg" alt="Posrter of native woman, Illustration by Dylan Petrohilos" width="1200"/>   
+<picture class="animate__fadeIn animate__animated">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_3.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_3.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_3.jpg" alt="Posrter of native woman, Illustration by Dylan Petrohilos" width="553" height="1129">
+</picture>
 </div>
 <div>
-<img class="animate__fadeIn animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_4.jpg" alt="Look Book For DC Action Lab, Photo of Noor Mir"  width="1200"/>   
-</div>
+<picture class="animate__fadeIn animate__animated">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_4.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_4.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_4.jpg" alt="Look Book For DC Action Lab, Photo of Noor Mir" width="1200" height="600">
+</picture></div>
 <div>
-<img class="multiply animate__fadeIn animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_7.jpg" alt="Graphic Poster"  width="800"/>   
-</div>
+<picture class="animate__fadeIn animate__animated multiply">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_7.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_7.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_7.jpg" alt="Sketches and drawings of different logo concepts" width="1200" height="1200">
+</picture></div>
 <div>
-<img class="multiply animate__fadeIn animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_8.jpg" alt="Graphic Poster"  width="1200"/>   
+<picture class="animate__fadeIn animate__animated multiply">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_8.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_8.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_8.jpg" alt="Group of people meeting over brand direction" width="1200" height="1200">
+</picture>
 </div>
 <div id="vision" class="content sub-item"><div>
     <h2 class="animate__fadeInDownBig animate__animated">Vision</h2>
@@ -51,7 +74,11 @@
 </div>    
 <div class="grid-item-3 grid-item">    
 <div>
-<img class="multiply animate__fadeIn animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_9.jpg" alt="Graphic Poster"  width="1200"/>   
+<picture class="animate__fadeIn animate__animated multiply">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_9.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_9.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_9.jpg" alt="Group of people working and discussing brand direction" width="1200" height="1200">
+</picture>
 </div>
 <div class="content animate__fadeIn animate__animated">
     <h2>We move people to action</h2>
@@ -66,7 +93,11 @@
 <div class="grid-item-4 grid-item">    
 
 <div>
-<img class="multiply animate__fadeIn animate__animated" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Image_12.jpg" alt="Graphic Poster"  width="1200"/>   
+<picture class="animate__fadeIn animate__animated multiply">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_12.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_12.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Front_Img_12.jpg" alt="Hands drawing logos" width="1200" height="1200">
+</picture>
 </div>
 <div class="content animate__fadeIn animate__animated">
     <h3>We offer</h3>
@@ -91,18 +122,59 @@ SEO, <br>
     <div class="logo_wrapper animate__fadeIn animate__animated">
         
         <div>
-        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_1.jpg" alt="clients"  width="1200"/>
+<picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_1.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_1.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_1.jpg" alt="Democracy Collaborative" width="400" height="400">
+</picture>
+        </div>
+        <div>
+<picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_2.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_2.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_2.jpg" alt="Google" width="400" height="400">
+</picture>
+        </div>
+        <div>
+<picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_3.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_3.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_3.jpg" alt="Think Progress" width="400" height="400">
+</picture>
         </div>
         
-        <div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_2.jpg" alt="clients"  width="1200"/>  
+        <div>
+<picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_4.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_4.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_4.jpg" alt="People For The American Way" width="400" height="400">
+</picture>
+        
         </div>
         
-        <div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_3.jpg" alt="clients"  width="1200"/>  
-        </div>
+        <div><picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_5.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_5.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_5.jpg" alt="Nat Geo" width="400" height="400">
+</picture>
+</div>
         
-        <div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_4.jpg" alt="clients"  width="1200"/> </div>
-        
-        <div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_5.jpg" alt="clients"  width="1200"/> </div><div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_6.jpg" alt="clients"  width="1200"/> </div><div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_7.jpg" alt="clients"  width="1200"/> </div><div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_8.jpg" alt="clients"  width="1200"/> </div>
+        <div><picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_6.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_6.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_6.jpg" alt="UCLA" width="400" height="400">
+</picture>
+</div><div><picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_7.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_7.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_7.jpg" alt="Center For American Progress" width="400" height="400">
+</picture>
+</div><div><picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_8.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_8.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/client_8.jpg" alt="Womens March" width="400" height="400">
+</picture>
+ </div>
         
     </div>
     </div> 
@@ -126,9 +198,24 @@ SEO, <br>
  
 </div>
 <div id="projects" class="content clients_wrapper grid-item-7 grid-item">
-    <div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work1.jpg" alt="Graphic Poster"  width="1200"/> </div>  
-   <div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work2.jpg" alt="Graphic Poster"  width="1200"/></div>  
-       <div><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work3.jpg" alt="Graphic Poster"  width="1200"/>    </div>
+    <div><picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work1.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work1.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work1.jpg" alt="Powershift Network" width="1200" height="1600">
+</picture>
+</div>  
+   <div><picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work2.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work2.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work2.jpg" alt="Hoco Solar Farm" width="1200" height="1600">
+</picture>
+</div>  
+       <div><picture>
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work3.webp" type="image/webp">
+  <source srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work3.jpg" type="image/jpeg"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Work3.jpg" alt="DC Action Lab" width="1200" height="1600">
+</picture>
+   </div>
     
 </div> 
 <?php get_footer(); ?>
