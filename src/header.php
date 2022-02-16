55d340d6545f704c35a74f3c3686a000bdd4aae3
dpetrohilos <dylanpetrohilos@gmail.com>
2022-02-16 15:09:05 -0500

    google

--- src/header.php
+++ src/header.php
@@ -5,7 +5,6 @@
 		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
     
 
-        <link href="//www.google-analytics.com" rel="dns-prefetch">
 		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/favicon.ico" rel="shortcut icon">
 		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
